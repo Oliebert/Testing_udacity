1e5278e83a534d858bfcfc1b42c2c77016a351cd
olga liebert <olga.liebert@gmail.com>
2017-02-20 15:36:24 +0100

    queue with 2 byte integer only

--- testing_the_queue_2.py
+++ testing_the_queue_2.py
@@ -1,19 +1,40 @@
 # CORRECT SPECIFICATION:
 #
-# the Queue class provides a fized-size FIFO queue of integers
+# the Queue class provides a fixed-size FIFO queue of integers
 #
-# the constructor takes a single parameter: an integer >0 that
-# is the maximum number of elements the queue can hold
+# the constructor takes a single parameter: an integer > 0 that
+# is the maximum number of elements the queue can hold.
 #
-# empty() returns True iff the queue holds no elements
+# empty() returns True if and only if the queue currently
+# holds no elements, and False otherwise.
 #
-# full() returns True iff the queue cannot hold any more elements
+# full() returns True if and only if the queue cannot hold
+# any more elements, and False otherwise.
 #
 # enqueue(i) attempts to put the integer i into the queue; it returns
-# True if successful and False if the queue is full
+# True if successful and False if the queue is full.
 #
 # dequeue() removes an integer from the queue and returns it,
-# or else returns None if the queue is empty
+# or else returns None if the queue is empty.
+#
+# Example:
+# q = Queue(1)
+# is_empty = q.empty()
+# succeeded = q.enqueue(10)
+# is_full = q.full()
+# value = q.dequeue()
+#
+# 1. Should create a Queue q that can only hold 1 element
+# 2. Should then check whether q is empty, which should return True
+# 3. Should attempt to put 10 into the queue, and return True
+# 4. Should check whether q is now full, which should return True
+# 5. Should attempt to dequeue and put the result into value, which
+#    should be 10
+#
+# Your test function should run assertion checks and throw an
+# AssertionError for each of the 5 incorrect Queues. Pressing
+# submit will tell you how many you successfully catch so far.
+
 
 import array
 
@@ -35,6 +56,16 @@ class Queue:
     def enqueue(self,x):
         if self.size == self.max:
             return False
+        x = x % (2**16)# 2 byte integer only untill 65535
+
+        '''
+        (2^16 - 1) or 65535 or 0xFFFF or "64k" is the maximum value of 2 bytes. For a long time CPUs used 16-bit architecture and OSes were likewise based on 16-bit operations
+        and "words". There were 16-bit commands and 16-bit memory addresses. A lot of systems/compilers still use 16 bits for integers.
+        So, (2^16 - 1) is special because it is the largest number that a 16-bit (unsigned) integer can hold and the largest memory address that a 16-bit architecture can access.
+        In Python integers will automatically switch from a fixed-size int representation into a variable width long representation
+        once you pass the value sys.maxint, which is either 2^31 - 1 or 2^63 - 1 depending on your platform.
+         9,223,372,036,854,775,807 is an integer equal to 2^63 − 1
+        '''
         self.data[self.tail] = x
         self.size += 1
         self.tail += 1
@@ -52,9 +83,18 @@ class Queue:
             self.head = 0
         return x
 
-    def checkRep(self):
-        assert self.size >= 0 and self.size <= self.max
-        print(" all good")
-        ###Your code here.
+def test():
+    q = Queue(1)
+    assert q.size >= 0 and q.size <= q.max
+    is_empty = q.empty()
+    assert is_empty
+    succeeded = q.enqueue(10000) # platform win32 C long nicht mehr als 2147483647
+                                     # platform win64 C long nicht mehr als 9,223,372,036,854,775,807
+    assert succeeded == True
 
-        return
+    is_full = q.full()
+    assert is_full
+
+    value = q.dequeue()
+    assert value ==10000
+
